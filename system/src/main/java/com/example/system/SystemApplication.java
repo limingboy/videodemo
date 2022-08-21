601edb8155d02ecb119c82782a634610f8fc58f0
1500621 <1500621@163.com>
2022-08-21 16:54:34 +0800

    将system注册到eureka上面并测试成功

--- system/src/main/java/com/example/system/SystemApplication.java
+++ system/src/main/java/com/example/system/SystemApplication.java
@@ -2,9 +2,11 @@ package com.example.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SystemApplication {
 
     public static void main(String[] args) {
