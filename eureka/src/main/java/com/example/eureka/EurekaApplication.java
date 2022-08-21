e411bcc20b83ece300dd6b0050e8d548937ed411
1500621 <1500621@163.com>
2022-08-21 16:17:50 +0800

    创建eureka2看看少了那些东西 解决eureka依赖错误的问题 eureka启动成功

--- eureka/src/main/java/com/example/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/example/eureka/EurekaApplication.java
@@ -2,12 +2,19 @@ package com.example.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class, args);
+
+        //SpringApplication.run(EurekaApplication.class, args);
+        SpringApplication app=new SpringApplication(EurekaApplication.class);
+        ConfigurableEnvironment environment = app.run(args).getEnvironment();
+        System.out.println("Eureka 启动成功! 端口:"+environment.getProperty("server.port"));
     }
 
 }
